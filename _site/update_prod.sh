87a65dd8c8fc2ce8d1697fb20017a00e1014f128
philcryer <phil@philcryer.com>
2015-04-06 14:47:04 -0500

    Published updates on Mon Apr  6 14:47:04 CDT 2015

---
+++ _site/update_prod.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+
+SELF=`basename $0`
+SOURCE_BRANCH="master"
+DEST_BRANCH="gh-pages"
+TMP_LOG=`git log | head -n1 | cut -d" " -f2`
+TMP_DIR="tmp-$TMP_LOG"
+
+git checkout $SOURCE_BRANCH
+jekyll build -d $TMP_DIR
+git checkout $DEST_BRANCH
+git rm -qr .
+cp -r $TMP_DIR/. .
+rm ./$SELF
+rm -r $TMP_DIR
+git add -A
+git commit -m "Published updates on `date`"
+git push origin $DEST_BRANCH
+git checkout $SOURCE_BRANCH
+
+exit 0
